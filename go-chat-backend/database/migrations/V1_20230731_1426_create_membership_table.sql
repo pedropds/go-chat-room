0b3683ff0d45d842da9178f6e0884c14a23f174a
pedropds <pedropintodsousa@gmail.com>
2023-08-03 11:34:11 +0100

    Small Change to script

--- go-chat-backend/database/migrations/V1_20230731_1426_create_membership_table.sql
+++ go-chat-backend/database/migrations/V1_20230731_1426_create_membership_table.sql
@@ -4,7 +4,7 @@ CREATE SEQUENCE IF NOT EXISTS membership_id_seq START 1 INCREMENT 1;
 -- Create the Membership Table
 CREATE TABLE membership (
                             membership_id INTEGER PRIMARY KEY DEFAULT nextval('membership_id_seq'),
-                            user_id INTEGER NOT NULL REFERENCES appuser(user_id),
                             room_id INTEGER NOT NULL REFERENCES chat_room(room_id),
+                            user_id INTEGER NOT NULL REFERENCES appuser(user_id),
                             joined_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
