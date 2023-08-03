51245e8e429794c4b7b0d45e90c04d4ae867a532
pedropds <pedropintodsousa@gmail.com>
2023-08-03 11:36:14 +0100

    Added chatroom to router

--- go-chat-backend/router/router.go
+++ go-chat-backend/router/router.go
@@ -21,6 +21,11 @@ func Init(db *gorm.DB) *gin.Engine {
 	var messageService service.MessageService = &service.MessageServiceImpl{Repository: messageRepository}
 	var messageController controller.MessageController = &controller.MessageControllerImpl{Service: messageService}
 
+	// create ChatRoom Dependencies
+	var chatRoomRepository model.ChatRoomRepository = &model.ChatRoomRepositoryImpl{Db: db}
+	var chatRoomService service.ChatRoomService = &service.ChatRoomServiceImpl{Repository: chatRoomRepository}
+	var chatRoomController controller.ChatRoomController = &controller.ChatRoomControllerImpl{Service: chatRoomService}
+
 	//User endpoints
 	r.POST("/user/login", userController.Login)
 
@@ -34,6 +39,9 @@ func Init(db *gorm.DB) *gin.Engine {
 
 	//Message endpoints
 	r.GET("/message/:roomId", messageController.GetAllMessagesForRoom)
+
+	//ChatRoom endpoints
+	r.GET("/chatroom/:userId", chatRoomController.GetAllChatRoomsForUser)
 
 	return r
 }
