71257635ae8dc4e3b56d0b66d28161fe49a32c16
pedropds <pedropintodsousa@gmail.com>
2023-08-04 11:56:31 +0100

    added .sample.env file and loading variables from same file

--- go-chat-backend/model/model.go
+++ go-chat-backend/model/model.go
@@ -1,15 +1,34 @@
 package model
 
 import (
+	"fmt"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"os"
 )
 
 func Setup() *gorm.DB {
 	print("Setting up database...\n")
 
-	var err error
-	dsn := "host=localhost user=postgres password=secret dbname=go_chat_room port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic("Failed to load env file! Error: " + err.Error())
+	}
+
+	postgresHost := os.Getenv("POSTGRES_HOST")
+	postgresPort := os.Getenv("POSTGRES_PORT")
+	postgresDB := os.Getenv("POSTGRES_DB")
+	postgresUser := os.Getenv("POSTGRES_USER")
+	postgresPassword := os.Getenv("POSTGRES_PASSWORD")
+	postgresSSLMode := os.Getenv("POSTGRES_SSL_MODE")
+	postgresTimezone := os.Getenv("POSTGRES_TIMEZONE")
+
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s TimeZone=%s",
+		postgresHost, postgresPort, postgresUser, postgresPassword, postgresDB, postgresSSLMode, postgresTimezone,
+	)
+
 	dbCon, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
