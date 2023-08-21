360535efc0daae6aa15ba815735541c218715ce7
pedropds <pedropintodsousa@gmail.com>
2023-08-21 22:20:49 +0100

    Added new message model

--- go-chat-frontend/src/model/chat.model.tsx
+++ go-chat-frontend/src/model/chat.model.tsx
@@ -1,6 +1,14 @@
-export default interface ChatRoomDTO {
+export interface ChatRoomDTO {
     roomId: number;
     roomName: string;
     createdAt: string;
     creatorId: number;
+}
+
+export interface ChatMessageDTO {
+    messageId: number;
+    roomId: number;
+    userId: number;
+    content: string;
+    createdAt: string;
 }
