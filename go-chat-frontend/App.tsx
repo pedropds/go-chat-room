9c47b84b560f6dc0b2a2b5806ab5d60f6e7ec4b6
pedropds <pedropintodsousa@gmail.com>
2023-08-29 18:56:17 +0100

    Change to how app works. Login page now active

--- go-chat-frontend/App.tsx
+++ go-chat-frontend/App.tsx
@@ -1,10 +1,46 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import MainContainer from './src/components/MainContainer';
+import { useEffect, useState } from 'react';
+import LoginScreen from './src/components/LoginPage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 export default function App() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
+  useEffect(() => {
+    const checkAuthentication = async () => {
+      try {
+        const storedValue = await AsyncStorage.getItem('isLoggedIn');
+        if (storedValue === 'true') {
+          setIsLoggedIn(false);
+        }
+        setLoading(false);
+      } catch (error) {
+        // Handle error
+      }
+    };
+    checkAuthentication();
+  }, []);
+
+  if (loading) {
+    return <Text></Text>
+  }
+
   return (
-     <MainContainer/>
+    <View style={{ flex: 1 }}>
+      {isLoggedIn ? (
+        <MainContainer />
+      ) : (
+        <LoginScreen onLogin={handleLogin} />
+      )}
+    </View>
   );
 }
 
