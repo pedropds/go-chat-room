a713cc2a36e3bbc95211c1a1d6365dbbd9892810
pedropds <pedropintodsousa@gmail.com>
2023-09-28 21:57:42 +0100

    added tables for friend requests

---
+++ go-chat-backend/database/migrations/V1_20230927_0956_create_friendship_requests_table.sql
@@ -0,0 +1,11 @@
+-- SEQ DEFINITION
+CREATE SEQUENCE IF NOT EXISTS friendship_id_seq START 1 INCREMENT 1;
+
+-- Create the Friendship Table
+CREATE TABLE friendships (
+                             friendship_id INTEGER PRIMARY KEY DEFAULT nextval('friendship_id_seq'),
+                             user1_id INTEGER NOT NULL REFERENCES appuser(user_id),
+                             user2_id INTEGER NOT NULL REFERENCES appuser(user_id),
+                             created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                             UNIQUE (user1_id, user2_id) -- Ensures that friendships are unique (no duplicates)
+);
---
+++ go-chat-backend/database/migrations/V1_20230927_0956_create_friendship_table.sql
@@ -0,0 +1,11 @@
+-- SEQ DEFINITION
+CREATE SEQUENCE IF NOT EXISTS friendship_request_id_seq START 1 INCREMENT 1;
+
+-- Create the Friendship Request Table
+CREATE TABLE friendship_requests (
+                                     request_id INTEGER PRIMARY KEY DEFAULT nextval('message_id_seq'),
+                                     sender_id INTEGER NOT NULL REFERENCES appuser(user_id),
+                                     receiver_id INTEGER NOT NULL REFERENCES appuser(user_id),
+                                     status VARCHAR(50) NOT NULL, -- Status can be "pending," "accepted," or "rejected"
+                                     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
